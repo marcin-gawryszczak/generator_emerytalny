6da617f33a5f6760f36f506ca0a22874143de80a
marcin <gawryszczakmarcin@gmail.com>
2023-01-19 12:34:29 +0100

    adding compound interest to generator

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-#generator_emerytalny version 1.0.0
+#generator_emerytalny version 1.2.0
 
 #Page which help user to count how much money should save for retirement
+
--- emerytura.js
+++ emerytura.js
@@ -1,11 +1,10 @@
-
 // do przycisku 'pokaż wyniki' dodajemy obsługę kliknięcia
 
 document.querySelector('#result').addEventListener('click', count_pension)
 
 
 
-
+let totalcash;
 
 function count_pension()
 {
@@ -14,12 +13,34 @@ function count_pension()
     let agelife = document.querySelector('#agelife').value;
     let monthlycash = document.querySelector('#monthlycash').value;
     let percent = document.querySelector('#percent').value;
+    let total_months_of_using_money = (agelife-pension_age)*12;
 
-    let investing_time = pension_age-age;
-    let using_fund_time_months = (agelife-pension_age)*12;
-    totalcash = 12*investing_time*monthlycash
-    pension = totalcash/using_fund_time_months
+    let investing_time_months = (pension_age-age)*12;
+    investing_money(percent, monthlycash, investing_time_months)
 
-    document.getElementById('pension').innerHTML = 'Twoja emerytura wyniesie: '+ pension+ ' zł'
+    
 
+    let pension = totalcash/total_months_of_using_money;
+
+    pension = pension.toFixed(2)
+
+    let statement = '<div>Twoja emerytura wyniesie: '+pension+ 'zł</div>'
+    console.log(statement)
+    document.querySelector('#pension').innerHTML = statement
+
+}
+
+function investing_money(profit,cash,time)
+{
+    // console.log(investing_time_months)
+    let total_investing_money = 0;
+    for (time; time>0;time--)
+    {
+        let monthlyprofitcash = cash* Math.pow((1+profit/100/12),time)
+        total_investing_money = total_investing_money +monthlyprofitcash
+        // console.log(total_investing_money)
+    }
+
+    totalcash = total_investing_money
+    
 }

e40f4e97b29fc80d2e37ff32882e6e856ae32b45
marcin <gawryszczakmarcin@gmail.com>
2023-01-23 22:35:53 +0100

    investing_in_retirement added

--- emerytura.js
+++ emerytura.js
@@ -3,6 +3,9 @@
 document.querySelector('#result').addEventListener('click', count_pension)
 
 let totalcash;
+let percent;
+let total_months_of_using_money
+let pension
 
 function count_pension()
 {
@@ -10,23 +13,24 @@ function count_pension()
     let pension_age = document.querySelector('#pension_age').value;
     let agelife = document.querySelector('#agelife').value;
     let monthlycash = document.querySelector('#monthlycash').value;
-    let percent = document.querySelector('#percent').value;
-    let total_months_of_using_money = (agelife-pension_age)*12;
+    percent = document.querySelector('#percent').value;
+    total_months_of_using_money = (agelife-pension_age)*12;
 
     let investing_time_months = (pension_age-age)*12;
+    
+    // if (percent != '0')
     investing_money(percent, monthlycash, investing_time_months)
+    
+    pension = totalcash/total_months_of_using_money;
 
     investing_in_retirement()
-
-
-    let pension = totalcash/total_months_of_using_money;
 
     pension = pension.toFixed(2)
     totalcash = totalcash.toFixed(2)
 
     let statement = '<div>Zgromadzisz środki w wysokości: '+totalcash+ 'zł</div>'
     statement += '<div>Twoja emerytura wyniesie: '+pension+ 'zł</div>'
-    console.log(statement)
+    // console.log(statement)
     document.querySelector('#pension').innerHTML = statement
 
 }
@@ -48,18 +52,32 @@ function investing_money(profit,cash,time)
 function investing_in_retirement()
 {
     const radioButtons = document.querySelectorAll('input[name="answer"]');
-
     let selectedanswer
     for (const radioButton of radioButtons)
     {
         if (radioButton.checked)
         {
             selectedanswer = radioButton.value;
-            break;
         }
     } 
 
-    console.log(selectedanswer)
+    if (selectedanswer == '0');
+    else {
+    let first_month_interest = totalcash*percent/100/12
+
+    console.log(first_month_interest)
+    
+    first_month_capital_pension = totalcash/((Math.pow((1+(percent/100/12)),total_months_of_using_money)-1)/(percent/100/12))
+
+    // console.log(first_month_capital_pension)
+
+    pension = first_month_capital_pension+first_month_interest
+
+
+}
+
+    
+
 }
 
 
